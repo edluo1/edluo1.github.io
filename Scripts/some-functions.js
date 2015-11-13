10a6ed80e564056b4b207d70e749fa9c5dec4b5f
Eddy Luo <edluo1@bu.edu>
2015-11-13 10:41:04 -0800

    Adding some extra words
    And a pictures

---
+++ Scripts/some-functions.js
@@ -0,0 +1,16 @@
+﻿function findBootstrapEnvironment() {
+    var envs = ['xs', 'sm', 'md', 'lg'];
+
+    var $el = $('<div>');
+    $el.appendTo($('body'));
+
+    for (var i = envs.length - 1; i >= 0; i--) {
+        var env = envs[i];
+
+        $el.addClass('hidden-' + env);
+        if ($el.is(':hidden')) {
+            $el.remove();
+            return env;
+        }
+    }
+}
