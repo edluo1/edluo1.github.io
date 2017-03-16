a85eb17a1f14404cd75c5db2a56af3e76ffaf8a0
Eddy Luo <edluo1@bu.edu>
2017-03-16 19:14:32 -0400

    Removed floating-point errors

--- feCalc/dmgCalc.js
+++ feCalc/dmgCalc.js
@@ -2,37 +2,36 @@ DmgFunc = {
 
 	trueHit: function(hit) { // chance for average of 2 RN
 		if (hit <= 50) {
-			return (2*(hit*hit)+hit)/100;
+			return Fraction((2*(hit*hit)+hit),100);
 		} else {
 			var m = hit-50;
-			return (5050 + 199*m - 2*m*m)/100;
+			return Fraction((5050 + 199*m - 2*m*m),100);
 		}
 	},
 
-	fatesHit: function(hit) { // chance for (3A+B)/4 RNG for hit > 50
+	fatesHit: function(hit) { // chance for (3A+B)/4 RNG for hit >= 50
 		if (hit < 50) return hit;
 		else if (hit <= 100) { // can't write formulas so i'll just do this
 			var resultsBkwd = [10000, 9999, 9993, 9982, 9965, 9943, 9916, 9883, 9845, 9802, 9753, 9699, 9640, 9575, 9505, 9430, 9349, 9263, 9172, 9075, 8973, 8866, 8753, 8635, 8512, 8383, 8250, 8117, 7983, 7850, 7717, 7583, 7450, 7317, 7183, 7050, 6917, 6783, 6650, 6517, 6383, 6250, 6117, 5983, 5850, 5717, 5583, 5450, 5317, 5183, 5050];
 			var m = 100-hit;
-			return (resultsBkwd[m]/100.0);
-		} else return hit; // no invalid array access
+			return Fraction(resultsBkwd[m],100);
+		} else return Fraction(hit); // no invalid array access
 	},
 
 	chanceTable: function (hit, dmg, crit, truehit) {
 		/* hit rate and crit rate : n out of 100. dmg is value */
 		/* returns: object containing all damage possibilities */
-		console.log(truehit);
 		if (truehit === 'truehit') {
 			hit = DmgFunc.trueHit(hit);
 		} else if (truehit === 'fates_hit') {
 			hit = DmgFunc.fatesHit(hit);
 		} // otherwise leave it as is
-		var critChance = (hit*crit)/(100*1.0);
+		var critChance = Fraction(Fraction(hit).mul(crit),(100*1.0));
 		var ct = [
-			{damage: 0, damageReceived: 0, chance: 100-hit},
-			{damage: dmg, damageReceived: 0, chance: hit-critChance}
+			{damage: 0, damageReceived: 0, chance: Fraction(100-hit)},
+			{damage: dmg, damageReceived: 0, chance: Fraction(hit).sub(critChance)}
 		];
-		if (critChance !== 0) ct.push({damage: 3*dmg, damageReceived: 0, chance: critChance});
+		if (critChance !== 0) ct.push({damage: 3*dmg, damageReceived: 0, chance: Fraction(critChance)});
 		return ct;
 	},
 
@@ -48,13 +47,13 @@ DmgFunc = {
 				chanceTable.push(t1[i]); 
 			} else {
 				for (var j = 0; j < t2.length; j++) {
-					var combineChance = (t1[i].chance*t2[j].chance)/(100*1.0);
+					var combineChance = Fraction((t1[i].chance.mul(t2[j].chance))).div(100);
 					var chanceObject = { // resulting table after counterattack
 						damage: t1[i].damage, 
 						damageReceived: +t1[i].damageReceived+t2[j].damage, 
 						chance: combineChance
 					};
-					if (combineChance !== 0) chanceTable.push(chanceObject);
+					if (!combineChance.equals(0)) chanceTable.push(chanceObject);
 				}
 			}
 		}
@@ -75,13 +74,13 @@ DmgFunc = {
 			}
 			else {
 				for (var j = 0; j < t2.length; j++) {
-					var combineChance = (t1[i].chance*t2[j].chance)/(100*1.0);
+					var combineChance = Fraction((t1[i].chance.mul(t2[j].chance))).div(100);
 					var chanceObject = {
 						damage: +t1[i].damage+t2[j].damage, 
 						damageReceived: t1[i].damageReceived, 
 						chance: combineChance
 					};
-					if (combineChance !== 0) chanceTable.push(chanceObject);
+					if (!combineChance.equals(0)) chanceTable.push(chanceObject);
 				}
 			}
 		}
@@ -130,7 +129,9 @@ DmgFunc = {
 		  	hash[key] = e;
 		  	r.push(hash[key]);
 		  }
-		  else hash[key].chance += e.chance;
+		  else {
+		  	hash[key].chance.add(e.chance);
+		  }
 		  return r;
 		}, []);
 		return result;
